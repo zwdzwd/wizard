c831972f4d5166810e1cc236be0d858277292d69
Wanding Zhou <wanding.zhou@vai.org>
2016-12-13 16:56:25 -0500

    update to group_to_max_sorted

--- R/affy.R
+++ R/affy.R
@@ -7,3 +7,12 @@
 #' @useDynLib wzard group_to_median_sorted_
 #' @export
 group.to.median.sorted <- function(x, f) .Call(group_to_median_sorted_, x, f)
+
+#' group to max
+#'
+#' for preprocessing affymetrix SNP6 array
+#' @param x data
+#' @param f factor
+#' @useDynLib wzard group_to_max_sorted_
+#' @export
+group.to.max.sorted <- function(x, f) .Call(group_to_max_sorted_, x, f)
--- src/affy.c
+++ src/affy.c
@@ -34,7 +34,7 @@ SEXP group_to_median_sorted_(SEXP x, SEXP f) {
   double *rc = REAL(r);
   int lv = fc[0];
   int cnt = 0;
-  double obs[4];
+  double obs[4];                /* at most 4 replica for SNP6 */
 
   int i, lv_;
   for (i=0; i<LENGTH(x); ++i) {
@@ -56,3 +56,43 @@ SEXP group_to_median_sorted_(SEXP x, SEXP f) {
   return r;
 }
 
+SEXP group_to_max_sorted_(SEXP x, SEXP f) {
+
+  /* x must be sorted by f */
+  int nlevs = nlevels(f);
+
+  if (TYPEOF(x) != REALSXP)
+    error("First argument must be a numeric.");
+  if (!isFactor(f))
+    error("First argument must be a factor.");
+  
+  SEXP r = PROTECT(allocVector(REALSXP, nlevs));
+
+  int *fc = INTEGER(f);
+  double *xc = REAL(x);
+  double *rc = REAL(r);
+  int lv = fc[0];
+  int cnt = 0;
+  double max = 0;
+
+  int i, lv_;
+  for (i=0; i<LENGTH(x); ++i) {
+    lv_ = fc[i];
+    if (lv != lv_) {
+      rc[lv-1] = max;
+      lv = lv_; cnt = 0;
+    }
+
+    if (cnt==0 || max < xc[i])
+      max = xc[i];
+    cnt++;
+  }
+
+  /* clean up */
+  if (cnt >= 1)
+    rc[lv-1] = max;
+
+  setAttrib(r, R_NamesSymbol, getAttrib(f, R_LevelsSymbol));
+  UNPROTECT(1);
+  return r;
+}
